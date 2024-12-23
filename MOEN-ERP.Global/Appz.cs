603216194e8a93842315d2487d61bd6c9c2f8065
CarbaminoD <carbamino.top@gmail.com>
2024-12-23 23:45:17 +0700

    MOEN-ERP.Global

---
+++ MOEN-ERP.Global/Appz.cs
@@ -0,0 +1,73 @@
+﻿using MOEN_ERP.Models.Common;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+
+namespace MOEN_ERP
+{
+    public class Appz
+    {
+
+        HttpContext _httpContext;
+        public Appz(HttpContext httpContextAccessor)
+        {
+            _httpContext = httpContextAccessor;
+        }
+        public string CurrentSessionId
+        {
+            get
+            {
+                return _httpContext?.Session.Id ?? "";
+            }
+        }
+
+        public AccountModel CurrentSignInUser
+        {
+
+            get
+            {
+                var str = _httpContext?.Session.GetString("MOEN_ERP.USER_SES");
+                if (str == null && _httpContext != null)
+                {
+                    ClaimsPrincipal claimAccount = _httpContext.User;
+                    if (claimAccount.Identity.IsAuthenticated)
+                    {
+                        var currentUser = JsonSerializer.Deserialize<AccountModel>(claimAccount.Identity.Name);
+                        if (currentUser.User != null)
+                        {
+                            _httpContext.Session.SetString("MOEN_ERP.USER_SES", JsonSerializer.Serialize(currentUser));
+                            return currentUser;
+                        }
+                        else
+                        {
+                            return new AccountModel();
+                        }
+                        
+                    }
+                    else
+                    {
+                        return new AccountModel();
+                    }
+
+                }
+                else
+                {
+                    return str == null ? default : JsonSerializer.Deserialize<AccountModel>(str);
+                }
+
+            }
+            set
+            {
+                _httpContext.Session.SetString("MOEN_ERP.USER_SES", JsonSerializer.Serialize(value));
+            }
+
+        }
+
+
+    }
+
+}
