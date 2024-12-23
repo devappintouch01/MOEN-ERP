9d3e01e64ced41252fe2b2a492517805f5bc8c78
CarbaminoD <carbamino.top@gmail.com>
2024-12-23 23:47:16 +0700

    MOEN-ERP.Utilities

---
+++ MOEN-ERP.Utilities/AuthenSSO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOEN_ERP.Utilities
+{
+    public class AuthenSSO
+    {
+        public static string DecodingFromBase64(string value)
+        {
+            byte[] data = Convert.FromBase64String(value);
+            return Encoding.UTF8.GetString(data);
+        }
+
+        public static string EncodingToBase64(string value)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(value);
+            return Convert.ToBase64String(data);
+        }
+
+    }
+}
---
+++ MOEN-ERP.Utilities/PdfMerger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace MOEN_ERP.Utilities
+{
+    public class PdfMerger
+    {
+
+        public static byte[] MergePdfFiles(List<byte[]> pdfBytes)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Document document = new Document();
+                PdfCopy copy = new PdfCopy(document, ms);
+                document.Open();
+
+                foreach (var pdfBytesArray in pdfBytes)
+                {
+                    PdfReader reader = new PdfReader(pdfBytesArray);
+                    int pageCount = reader.NumberOfPages;
+                    for (int i = 0; i < pageCount;)
+                    {
+                        copy.AddPage(copy.GetImportedPage(reader, ++i));
+                    }
+
+                    copy.FreeReader(reader);
+                    reader.Close();
+                }
+
+                document.Close();
+                return ms.ToArray();
+            }
+        }
+
+
+    }
+}
